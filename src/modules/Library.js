71ef9f50b6a7d586283a669b41820078145c6e29
menulln <39300523+menulln@users.noreply.github.com>
2024-01-25 18:50:01 +0100

    Add deleteBook method

--- src/modules/Library.js
+++ src/modules/Library.js
@@ -6,4 +6,17 @@ export default class Library {
             this.books.push(book);
         });
     }
+
+    static deleteBook(title) {
+        this.books.forEach((book) => {
+            if (book.title === title) {
+                this.books.splice(
+                    this.books.findIndex(
+                        (currentBook) => currentBook.title === title
+                    ),
+                    1
+                );
+            }
+        });
+    }
 }
