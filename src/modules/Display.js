57a77322655ca767749753ff6da573f8c767e25c
menulln <39300523+menulln@users.noreply.github.com>
2024-01-25 18:52:34 +0100

    Add method renderBooks

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -69,4 +69,45 @@ export default class Display {
         const main = document.querySelector('main');
         main.appendChild(form);
     }
+
+    static renderBooks() {
+        const main = document.querySelector('main');
+
+        if (document.querySelector('.books')) {
+            document.querySelector('.books').remove();
+        }
+
+        const books = document.createElement('div');
+        books.classList.toggle('books');
+
+        Library.books.forEach((book) => {
+            const container = document.createElement('div');
+            const title = document.createElement('h3');
+            const author = document.createElement('p');
+            const pages = document.createElement('p');
+            const hasRead = document.createElement('div');
+
+            title.textContent = book.title;
+            author.textContent = book.author;
+            pages.textContent = book.pages;
+
+            title.style.display = 'inline';
+            author.style.display = 'inline';
+            pages.style.display = 'inline';
+            hasRead.style.display = 'inline-block';
+
+            hasRead.style.width = '20px';
+            hasRead.style.height = '20px';
+            hasRead.style.backgroundColor = book.hasRead ? 'green' : 'red';
+
+            container.appendChild(title);
+            container.appendChild(author);
+            container.appendChild(pages);
+            container.appendChild(hasRead);
+
+            books.appendChild(container);
+        });
+
+        main.appendChild(books);
+    }
 }
