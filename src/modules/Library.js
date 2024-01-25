7e8280c4680686fa021fc5e81dc08e8c0898bb14
menulln <39300523+menulln@users.noreply.github.com>
2024-01-25 18:49:51 +0100

    Add addBook method

--- src/modules/Library.js
+++ src/modules/Library.js
@@ -1,3 +1,9 @@
 export default class Library {
     static books = [];
+
+    static addBook(...books) {
+        books.forEach((book) => {
+            this.books.push(book);
+        });
+    }
 }
