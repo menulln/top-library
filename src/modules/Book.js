da4d30660d0648c162a243f0303f24ce42e5b5a3
menulln <39300523+menulln@users.noreply.github.com>
2024-01-25 18:49:14 +0100

    Export Book class

---
+++ src/modules/Book.js
@@ -0,0 +1,8 @@
+export default class Book {
+    constructor(title, author, pages, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.hasRead = hasRead;
+    }
+}
